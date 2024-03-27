79b9a1a6aeec942f714a350b5f31942b297ddca9
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2024-03-27 15:05:51 +0000

    Use the name for the ProductCategory ID

--- src/main/java/com/sportyshoes/entity/ProductCategory.java
+++ src/main/java/com/sportyshoes/entity/ProductCategory.java
@@ -1,11 +1,8 @@
 package com.sportyshoes.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import java.time.LocalDateTime;
 import java.util.List;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -22,10 +19,7 @@ import lombok.ToString;
 public class ProductCategory {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
   @EqualsAndHashCode.Include
-  private final LocalDateTime created = LocalDateTime.now();
   private String name;
 
   @OneToMany(mappedBy = "product_category")
