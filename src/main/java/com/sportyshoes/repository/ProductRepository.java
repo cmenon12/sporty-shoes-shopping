bba2372585eea5c06d99b8d6754dc2ba86f9db6f
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2024-03-27 16:25:13 +0000

    Override ProductRepository methods to exclude deleted products

--- src/main/java/com/sportyshoes/repository/ProductRepository.java
+++ src/main/java/com/sportyshoes/repository/ProductRepository.java
@@ -1,10 +1,20 @@
 package com.sportyshoes.repository;
 
 import com.sportyshoes.entity.Product;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+  @Query("select p from Product p where p.isDeleted = false")
+  List<Product> findAll();
+
+  @Query("select p from Product p where p.id = :id and p.isDeleted = false")
+  Optional<Product> findById(@Param("id") long id);
 
 }
