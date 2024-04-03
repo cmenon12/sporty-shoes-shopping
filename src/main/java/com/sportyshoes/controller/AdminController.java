eca8bae2da10a37f0ba013fa96ea9c1abdd45614
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2024-04-03 08:54:25 +0100

    Create editProduct and createProduct controller methods

--- src/main/java/com/sportyshoes/controller/AdminController.java
+++ src/main/java/com/sportyshoes/controller/AdminController.java
@@ -5,10 +5,12 @@ import com.sportyshoes.entity.User;
 import com.sportyshoes.service.ProductService;
 import jakarta.servlet.http.HttpSession;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -39,6 +41,39 @@ public class AdminController {
     }
     model.addAttribute("allProducts", allProducts);
     return "admin_products";
+  }
+
+  @GetMapping(value = "/products/{id}")
+  public String editProduct(@PathVariable("id") Integer id, Model model, HttpSession session) {
+    User user = (User) session.getAttribute("user");
+    if (user == null) {
+      return "redirect:/login";
+    }
+    if (!user.getIsAdmin()) {
+      return "redirect:/";
+    }
+    model.addAttribute("user", user);
+    Optional<Product> product = productService.getById(id);
+    if (product.isEmpty()) {
+      model.addAttribute("resultDanger", "Product with ID=" + id + " not found.");
+      return "redirect:/admin/products";
+    }
+    model.addAttribute("product", product.get());
+    return "admin_products_edit";
+  }
+
+  @GetMapping(value = "/products/new")
+  public String createProduct(Model model, HttpSession session) {
+    User user = (User) session.getAttribute("user");
+    if (user == null) {
+      return "redirect:/login";
+    }
+    if (!user.getIsAdmin()) {
+      return "redirect:/";
+    }
+    model.addAttribute("user", user);
+    model.addAttribute("product", null);
+    return "admin_products_edit";
   }
 
 }
